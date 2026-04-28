2ccb47cf3cee093282c5163b759e6a06f1527ebd
ContactEasyKarobar <contacteasykarobar@gmail.com>
2026-04-29 00:06:01 +0530

    Added CORS+new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import QRCode from "qrcode";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-const cors = require('cors');
+ 
 // __dirname fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,6 +25,13 @@ app.use(cors());
 app.use(express.json());
 
 const sessions = {};
+
+
+app.use(cors({
+  origin: "https://easykarobar.in",
+  methods: ["GET", "POST"]
+}));
+
 
 // ================= SESSION START =================
 async function startSession(userId) {
